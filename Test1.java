4308a658a979e93dd3425853f5101bde9de6e20f
Siyathandwa <219014086@ump.ac.za>
2023-03-30 02:03:42 +0200

    first commit of Test

---
+++ Test1.java
@@ -0,0 +1,10 @@
+public class AddTwoValues {
+
+    public static void main(String[] args) {
+        int value1 = 5;
+        int value2 = 3;
+        int sum = value1 + value2;
+        System.out.println("The sum of " + value1 + " and " + value2 + " is " + sum + ".");
+    }
+
+}
